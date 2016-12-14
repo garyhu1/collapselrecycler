423c3ef1d56a32bd498a2ad8b5b6f80bf480468d
garyhu <942984941@qq.com>
2016-12-14 22:32:32 +0800

    Behavior

---
+++ app/src/main/java/com/garyhu/citypickerdemo/widget/behaviordemo/FriendInfoAdapter.java
@@ -0,0 +1,40 @@
+package com.garyhu.citypickerdemo.widget.behaviordemo;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.garyhu.citypickerdemo.R;
+
+/**
+ * Created by cqll on 2016/9/30.
+ */
+
+public class FriendInfoAdapter extends RecyclerView.Adapter<FriendInfoAdapter.ViewHolder>{
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_friend_info,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView txt;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            txt= (TextView) itemView.findViewById(R.id.txt);
+        }
+    }
+
+}
