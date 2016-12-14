7200413cd49ca2120c196d7c43fae2a333bf719e
garyhu <942984941@qq.com>
2016-12-14 21:55:03 +0800

    增加折叠效果

--- app/src/main/java/com/garyhu/citypickerdemo/activity/CollpasedActivity.java
+++ app/src/main/java/com/garyhu/citypickerdemo/activity/CollpasedActivity.java
@@ -2,7 +2,9 @@ package com.garyhu.citypickerdemo.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.garyhu.citypickerdemo.R;
 
@@ -14,9 +16,18 @@ import com.garyhu.citypickerdemo.R;
 
 public class CollpasedActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+    private CollapsingToolbarLayout collaps;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collapse);
+
+        collaps = ((CollapsingToolbarLayout) findViewById(R.id.collapse_layout));
+//        collaps.setTitle("我下面给你吃");
+
+        toolbar = ((Toolbar) findViewById(R.id.tool_bar));
+        toolbar.setTitle("饿了么");
     }
 }
